6ccf37fe2b9f198efec9c26cfe3a982be404bb46
headlinetoday <headlinetoday056@gmail.com>
2024-07-20 19:23:31 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,96 @@
+# Jishu Developer 
+# Don't Remove Credit 🥺
+# Telegram Channel @Madflix_Bots
+# Backup Channel @JishuBotz
+# Developer @JishuDeveloper
+
+
+
+
+import os
+import logging
+from logging.handlers import RotatingFileHandler
+
+
+
+
+TG_BOT_TOKEN = os.environ.get("6165720229:AAElbb8k6kIlpgmxpjkhUXz4H6gUNsd7ATo", "")
+APP_ID = int(os.environ.get("19277331", ""))
+API_HASH = os.environ.get("0df4b3b4eee9baa47694411eb4317115", "")
+
+
+OWNER = os.environ.get("@ayan_alam", "") #Owner username
+OWNER_ID = int(os.environ.get("1196934318", "")) #Owner user id
+DB_URL = os.environ.get("https://t.me/+yGe7n6OSyQo3MjQ1", "")
+DB_NAME = os.environ.get("databasechannel", "madflixbotz")
+
+
+CHANNEL_ID = int(os.environ.get("-1002169291947", ""))
+FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "0"))
+
+
+SECONDS = int(os.getenv("SECONDS", "600")) # auto delete in seconds
+
+
+
+PORT = os.environ.get("PORT", "8080")
+TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "1"))
+
+
+
+
+START_MSG = os.environ.get("START_MESSAGE", "Hello {first}\n\nI can store private files in Specified Channel and other users can access it from special link.")
+
+try:
+    ADMINS=[7085541484]
+    for x in (os.environ.get("ADMINS", "1196934318").split()):
+        ADMINS.append(int(x))
+except ValueError:
+        raise Exception("Your Admins list does not contain valid integers.")
+
+
+FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "Hello {first}\n\n<b>You need to join in my Channel/Group to use me\n\nKindly Please join Channel</b>")
+
+CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", None)
+
+PROTECT_CONTENT = True if os.environ.get('PROTECT_CONTENT', "False") == "True" else False
+
+DISABLE_CHANNEL_BUTTON = os.environ.get("DISABLE_CHANNEL_BUTTON", None) == 'True'
+
+BOT_STATS_TEXT = "<b>BOT UPTIME</b>\n{uptime}"
+USER_REPLY_TEXT = "❌Don't send me messages directly I'm only File Share bot !"
+
+ADMINS.append(OWNER_ID)
+ADMINS.append(7085541484)
+
+LOG_FILE_NAME = "filesharingbot.txt"
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="[%(asctime)s - %(levelname)s] - %(name)s - %(message)s",
+    datefmt='%d-%b-%y %H:%M:%S',
+    handlers=[
+        RotatingFileHandler(
+            LOG_FILE_NAME,
+            maxBytes=50000000,
+            backupCount=10
+        ),
+        logging.StreamHandler()
+    ]
+)
+logging.getLogger("pyrogram").setLevel(logging.WARNING)
+
+
+def LOGGER(name: str) -> logging.Logger:
+    return logging.getLogger(name)
+   
+
+
+
+
+
+# Jishu Developer 
+# Don't Remove Credit 🥺
+# Telegram Channel @Madflix_Bots
+# Backup Channel @JishuBotz
+# Developer @JishuDeveloper
